b46bc490a319943dd9484b600f926121de56e75f
aardv <aardvarkchase@gmail.com>
2023-02-12 18:57:52 -0500

    Reducing bloat

--- SetOperations/src/Birthday.java
+++ SetOperations/src/Birthday.java
@@ -1,63 +1,52 @@
 package src;
 
+/**
+ * This program simulates the famous "Birthday Paradox"
+ * Prints the percentage to the console
+ *
+ * @author Alexander Mortillite
+ */
 import java.util.ArrayList;
 
 public class Birthday
 {
-    public static void main(String[] argsWH)
+    public static void main(String[] args)
     {
-        int classSize = 23;
-        int count = 0;
-        int timesRan = 1000000;
-        Birthday Bday = new Birthday();
+        int classSize = 23;                         //Number of people in the class
+        int count = 0;                              //Records the number of times that two people have the same birthday
+        int timesRan = 1000000;                     //Records the number of times that the simulation is going to loop
+        Birthday Bday = new Birthday();             //Creates a Birthday Object to avoid static methods
         for(int i = 0; i< timesRan;i++)
-        {
-            if(Bday.testLoop(classSize) == 1)
-            {
-                count++;
-            }
-        }
-        //Bday.testLoop();
-        System.out.println((double)((double)count/(double)timesRan)*100 +"%");
-    }
-    public int testLoop(int classSize)
-    {
+            if(Bday.testLoop(classSize) == true)       //I use a return value of true
+                count++;                            //Increment the count variable
 
-        double sameBirthday = 0;
-        int count = 0;
-        ArrayList<Person> classroom = new ArrayList<Person>();
+        System.out.println(((double)count/(double)timesRan)*100 +"%"); // Print the percentage
+    }
+    public boolean testLoop(int classSize)
+    {
+        boolean count = false;
+        ArrayList<Person> classroom = new ArrayList<>();
         for(int i = 0; i<classSize; i++)
-        {
             classroom.add(new Person());
-        }
-        //System.out.println(hasBirthday(classroom));
+
         if(hasBirthday(classroom))
-        {
-            count = 1;
-        }
+            count = true;
+
         return count;
     }
 
     public boolean hasBirthday(ArrayList<Person> classroom)
     {
-        boolean output = false;
+        boolean output = false;               //Sets the default return to false
         for(int i = 0; i < classroom.size(); i++)
-        {
             for(int j = 0; j < classroom.size(); j++)
-            {
                 if((classroom.get(i).getBirthday() == classroom.get(j).getBirthday()) && (i != j))
                 {
-                    //System.out.println("Person A bday: "+ i +" " + classroom.get(i).getBirthday() );
-                    //System.out.println("Person B bday: "+ j +" " + classroom.get(j).getBirthday() );
-
-                    output = true;
-                    i = classroom.size();
-                    j = classroom.size();
+                    output = true;             //Sets the return statement = true
+                    i = classroom.size();      //These two lines are added so the nested for loop exits early
+                    j = classroom.size();      //refer previous
                 }
-            }
-        }
+
         return output;
     }
-
 }
-
